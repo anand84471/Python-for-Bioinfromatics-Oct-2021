dc190534f2fd9071358cc301bb7bb54d03b7c7c5
parul6571 <bhattparul1@gmail.com>
2021-10-31 14:58:45 +0530

    commit

--- RMC_Assignments/biopython_assignment.py
+++
@@ -1,6 +0,0 @@
-Program to sort fasta file based on sequence length
-Program to sort fasta file based on gc percentage
-Splitting big fasta file in n no of files.
-Transcribe the whole genome
-Selecting sequences having particular motifs present from whole genome
-Translate the whole genome
